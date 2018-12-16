5fd5dd634ee3ed85621e1841f548c234b0550f10
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 19:27:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material
+    attr_reader :brand
+   def initialize(brand)
+     @title=title
+  end
+
+  
+  
+  def turn_page
+    puts "The shoe has been repaired"
+  end
+end
