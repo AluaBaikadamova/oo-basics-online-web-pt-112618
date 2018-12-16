ac65685086cad6bf7290d99cf320b1dff98a39f4
Alua Baikadamova <abaikad00@gmail.com>
2018-12-16 19:24:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,7 @@
 class Book
+  
+    attr_accessor :author, :page_count, :genre
+  
    def initialize(title="And Then There Were None")
      @title=title
   end
@@ -7,28 +10,7 @@ class Book
     @title
   end
   
-  def author=(author)
-    @author = author
-  end
-    def author
-    @author
-  end
   
-    def page_count=(num)
-    @page_count = num
-  end
- 
-  def page_count
-    @page_count
-  end
- 
-  def genre=(genre)
-    @genre = genre
-  end
- 
-  def genre
-    @genre
-  end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
